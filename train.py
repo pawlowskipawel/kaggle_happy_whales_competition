9475e2e26d40452677bf74e9d84edebd770c941a
pawlowskipawel <pawlowski_pawel@interia.pl>
2022-03-21 00:56:08 +0100

    remove mixed precision from training

--- train.py
+++ train.py
@@ -20,6 +20,7 @@ from happy_whales.models import *
 from happy_whales.metrics import *
 
 import random
+import pickle
 import glob
 import os
 
@@ -38,9 +39,7 @@ def fix_species(species_col):
                                 "kiler_whale": "killer_whale",
                                 "bottlenose_dolpin": "bottlenose_dolphin"})
 
-def train_epoch(epoch, model, criterion, optimizer, train_dataloader, valid_dataloader=None, lr_scheduler=None, amp=False, scaler=None, device="cuda"):
-    
-    assert (amp and scaler) or not (amp and scaler), "Wrong setting amp/scaler!"
+def train_epoch(epoch, model, criterion, optimizer, train_dataloader, valid_dataloader=None, lr_scheduler=None, device="cuda"):
     
     model.train()
     
@@ -50,7 +49,6 @@ def train_epoch(epoch, model, criterion, optimizer, train_dataloader, valid_data
         progress_bar.set_description(f"Epoch {epoch+1}".ljust(25))
         
         for step, batch in enumerate(progress_bar, 1):
-            
             # More efficient than optimizer.zero_grad()            
             for p in model.parameters():
                 p.grad = None
@@ -58,48 +56,30 @@ def train_epoch(epoch, model, criterion, optimizer, train_dataloader, valid_data
             images = batch["image"].to(device)
             labels = batch["label"].to(device)
             
-            #images = images.permute(0, 3, 1, 2)
+            outputs = model(images, labels, return_embeddings=True)
             
-            if amp:
-                with torch.cuda.amp.autocast():
-                    outputs = model(images, labels, return_embeddings=True)
-                    
-                    embeddings = outputs["embeddings"]
-                    logits = outputs["logits"]
-                    loss = criterion(logits, labels)
-                    
-                scaler.scale(loss).backward()
-                scaler.step(optimizer)
-                scaler.update()
-                
-            else:
-                outputs = model(images, return_embeddings=True)
-                
-                embeddings = outputs["embeddings"]
-                logits = outputs["logits"]
-                
-                loss = criterion(logits, labels)
+            embeddings = outputs["embeddings"]
+            logits = outputs["logits"]
+            
+            loss = criterion(logits, labels)
 
-                loss.backward()
-                optimizer.step()
-            
+            loss.backward()
+            optimizer.step()
+        
             batch_losses.append(loss.item())
             
             progress_bar.set_postfix({"train loss": np.array(batch_losses).mean()})
             
-            if valid_dataloader and lr_scheduler and (step % cfg.scheduler_step) == 0:
+            if valid_dataloader and lr_scheduler and (step % cfg.scheduler_step) == 0 and step > 0:
                 val_loss, val_accuracy = validate(epoch, model, valid_dataloader, criterion, amp=False, scaler=None, disable_bar=True, device=cfg.device)
                 lr_scheduler.step(val_loss)
-                model.train()
                 
         epoch_loss = np.array(batch_losses).mean()
     
     return epoch_loss
 
 @torch.no_grad()
-def validate(epoch, model, dataloader, criterion, amp=False, scaler=None, disable_bar=False, device="cuda"):
-    model.eval()
-    
+def validate(epoch, model, dataloader, criterion, disable_bar=False, device="cuda"):
     
     val_batch_losses = []
     val_batch_maps = []
@@ -115,24 +95,12 @@ def validate(epoch, model, dataloader, criterion, amp=False, scaler=None, disabl
             images = batch["image"].to(device)
             labels = batch["label"].to(device)
             
-            if amp:
-                with torch.cuda.amp.autocast():
-                    outputs = model(images, labels, return_embeddings=True)
-                    
-                    embeddings = outputs["embeddings"]
-                    logits = outputs["logits"]
-                    
-                    loss = criterion(logits, labels)
-                
-                loss = scaler.scale(loss)
-                
-            else:
-                outputs = model(images, labels, return_embeddings=True)
-                
-                embeddings = outputs["embeddings"]
-                logits = outputs["logits"]
-                
-                loss = criterion(logits, labels)
+            outputs = model(images, labels, return_embeddings=True)
+            
+            embeddings = outputs["embeddings"]
+            logits = outputs["logits"]
+            
+            loss = criterion(logits, labels)
                 
             sorted_outputs, sorted_predictions = torch.sort(logits, descending=True)
             
@@ -147,41 +115,60 @@ def validate(epoch, model, dataloader, criterion, amp=False, scaler=None, disabl
     
     return val_loss, val_accuracy
 
+def duplicate_ones(dataset):
+    id_counter = dataset["individual_id"].value_counts().to_frame()
+    only_one_df = dataset[dataset["individual_id"].isin(id_counter[id_counter["individual_id"] == 1].index)]
+    new_dataset = pd.concat([dataset, only_one_df], ignore_index=True)
+
+    return new_dataset
 
 class cfg:
     device = "cuda"
     epochs = 10
-    batch_size = 128
-    num_classes = 10
-    model_name = "efficientnet_b0"
-    image_shape = (224, 224)
-    learning_rate = 0.001
+    batch_size = 8
+    num_classes = 5000
+    model_name = "tf_efficientnet_b5"
+    image_shape = (456, 456)
+    learning_rate = 0.0001
+    minimum_learning_rate = 1e-7
     embedding_dim = 1280
-    scheduler_step = 5
-
+    scheduler_step = 10
+    find_lr = True
+    
 if __name__ == '__main__':
     seed_everything()
     
-    TRAIN_DATA_IMG_DIR = "data/train_images_512x512"
+    TRAIN_DATA_IMG_DIR = "data/train_images"
     TRAIN_DATA_ANNOTATION_PATH = "data/train.csv"
     
     dataset = pd.read_csv(TRAIN_DATA_ANNOTATION_PATH)
     dataset["species"] = fix_species(dataset["species"])
 
+    dataset = duplicate_ones(dataset)
+    
     dataset["image"] = TRAIN_DATA_IMG_DIR + '/' + dataset["image"]
     
-    label_encoder = LabelEncoder()
-    dataset['individual_id'] = label_encoder.fit_transform(dataset['individual_id'])
+    label_encoder_id = LabelEncoder()
+    label_encoder_species = LabelEncoder()
+    
+    dataset['individual_id'] = label_encoder_id.fit_transform(dataset['individual_id'])
+    dataset['species'] = label_encoder_species.fit_transform(dataset['species'])
+    
+    pickle.dump(label_encoder_id, open("label_encoder_id.pickle", "wb"))
+    pickle.dump(label_encoder_species, open("label_encoder_species.pickle", "wb"))
+    
+    bbox_dict = pickle.load(open("data/bbox_train.pickle", "rb"))
     
     dataset = extract_top_n_classes(dataset, cfg.num_classes)
     
-    train_df, valid_df = train_test_split(dataset, test_size=.15)
+    #train_df, valid_df = train_test_split(dataset, test_size=.4, stratify=dataset["individual_id"])
+    train_df, valid_df = train_test_split(dataset, test_size=.5, stratify=dataset["individual_id"])
     
-    train_dataset = HappyWhalesDataset(train_df, cfg.image_shape)
-    valid_dataset = HappyWhalesDataset(valid_df, cfg.image_shape)
+    train_dataset = HappyWhalesDataset(train_df, cfg.image_shape, bbox_dict=bbox_dict)
+    valid_dataset = HappyWhalesDataset(valid_df, cfg.image_shape, bbox_dict=bbox_dict)
     
     train_dataloader = DataLoader(train_dataset, batch_size=cfg.batch_size, pin_memory=True, num_workers=16)
-    valid_dataloader = DataLoader(valid_dataset, batch_size=64, pin_memory=True, num_workers=16)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=cfg.batch_size, pin_memory=True, num_workers=16)
 
     model = HappyWhalesModel(cfg.model_name, cfg.embedding_dim, cfg.num_classes).to(cfg.device)
     
@@ -189,10 +176,17 @@ if __name__ == '__main__':
     optimizer = optim.AdamW([{'params': model.parameters()}, 
                              {'params': criterion.parameters()}], 
                             lr=cfg.learning_rate)
-    
-    lr_scheduler = ReduceLROnPlateau(optimizer, min_lr=.00000001, verbose=True, patience=5)
+         
+    lr_scheduler = ReduceLROnPlateau(optimizer, min_lr=cfg.minimum_learning_rate, verbose=True)
     scaler = torch.cuda.amp.GradScaler()
-    for epoch in range(cfg.epochs):
-        loss = train_epoch(epoch, model, criterion, optimizer, train_dataloader, valid_dataloader, lr_scheduler, amp=True, scaler=scaler, device=cfg.device)
-        val_loss, val_accuracy = validate(epoch, model, valid_dataloader, criterion, amp=False, scaler=None, device=cfg.device)
     
+    best_val_acc = 0
+    
+    for epoch in range(cfg.epochs):
+        loss = train_epoch(epoch, model, criterion, optimizer, train_dataloader, valid_dataloader, None, device=cfg.device)
+        val_loss, val_accuracy = validate(epoch, model, valid_dataloader, criterion, device=cfg.device)
+        
+        if val_accuracy > best_val_acc:
+            best_val_acc = val_accuracy
+            
+            torch.save(model.state_dict(), f"model.pth")
