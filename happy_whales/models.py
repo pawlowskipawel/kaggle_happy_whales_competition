795c603c3ecd41ef3b80cc363452530f3a0f30e0
pawlowskipawel <pawlowski_pawel@interia.pl>
2022-03-28 22:43:33 +0200

    ArcMarginProduct weight init

--- happy_whales/models.py
+++ happy_whales/models.py
@@ -32,12 +32,7 @@ class ArcMarginProduct(nn.Module):
     def __init__(self, in_features, out_features):
         super().__init__()
         self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
-        self.reset_parameters()
-
-    def reset_parameters(self):
         nn.init.xavier_uniform_(self.weight)
-        # stdv = 1. / math.sqrt(self.weight.size(1))
-        # self.weight.data.uniform_(-stdv, stdv)
 
     def forward(self, features):
         cosine = F.linear(F.normalize(features), F.normalize(self.weight))
