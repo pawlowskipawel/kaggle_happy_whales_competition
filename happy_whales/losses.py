3959bebf1c9db0e311ecd947246b1482d5476086
pawlowskipawel <pawlowski_pawel@interia.pl>
2022-04-03 18:26:52 +0200

    ArcFaceLoss basic implementation

--- happy_whales/losses.py
+++ happy_whales/losses.py
@@ -25,23 +25,21 @@ class FocalLoss(nn.Module):
 
 # Cell
 class ArcFaceLoss(nn.Module):
-    def __init__(self, s=45.0, m=0.2, crit="focal", weight=None, reduction="mean", class_weights_norm=None):
+    def __init__(self, s, m, crit, easy_margin, label_smothing_eps=0):
         super().__init__()
 
-        self.weight = weight
-        self.reduction = reduction
-        self.class_weights_norm = class_weights_norm
+        self.easy_margin = easy_margin
+        self.label_smothing_eps = label_smothing_eps
 
         if crit == "focal":
             self.crit = FocalLoss()
         elif crit == "crossentropy":
-            self.crit = nn.CrossEntropyLoss(reduction="none")
+            self.crit = nn.CrossEntropyLoss()
 
         if s is None:
-            self.s = torch.nn.Parameter(torch.tensor([45.], requires_grad=True, device='cuda'))
+            self.s = torch.nn.Parameter(torch.tensor([30.], requires_grad=True, device='cuda'))
         else:
             self.s = s
-
 
         self.cos_m = math.cos(m)
         self.sin_m = math.sin(m)
@@ -54,33 +52,22 @@ class ArcFaceLoss(nn.Module):
         cosine = logits
         sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
         phi = cosine * self.cos_m - sine * self.sin_m
-        phi = torch.where(cosine > self.th, phi, cosine - self.mm)
 
+        if self.easy_margin:
+            phi = torch.where(cosine > 0, phi, cosine)
+        else:
+            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
+
+        # label one hot encoding
         labels2 = torch.zeros_like(cosine)
         labels2.scatter_(1, labels.view(-1, 1).long(), 1)
+
+        if self.label_smothing_eps > 0:
+            labels2 = (1 - self.label_smothing_eps) * labels2 + self.label_smothing_eps / self.num_classes
+
         output = (labels2 * phi) + ((1.0 - labels2) * cosine)
+        output *= self.s
 
-        s = self.s
-
-        output = output * s
         loss = self.crit(output, labels)
-
-        if self.weight is not None:
-            w = self.weight[labels].to(logits.device)
-
-            loss = loss * w
-            if self.class_weights_norm == "batch":
-                loss = loss.sum() / w.sum()
-            if self.class_weights_norm == "global":
-                loss = loss.mean()
-            else:
-                loss = loss.mean()
-
-            return loss
-
-        if self.reduction == "mean":
-            loss = loss.mean()
-        elif self.reduction == "sum":
-            loss = loss.sum()
 
         return loss
